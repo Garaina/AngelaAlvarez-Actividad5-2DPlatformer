90e1c45018e91583744a87292315d3c331cef50e
Angela Maria Alvarez <angelaalhe@unisabana.edu.co>
2018-05-05 21:19:22 -0500

    Version final mario

---
+++ AngelaAlvarez-2DPlatformer/Assets/ScriptCamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCamara : MonoBehaviour {
+
+	public Transform player;
+	public float offsetX;
+	public float offsetY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (player.transform.position.x + offsetX, offsetY, -10);
+		
+	}
+}
---
+++ AngelaAlvarez-2DPlatformer/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterControl : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	int coins = 0;
+
+	int hearts = 0;
+
+	public Text contadorCoins;
+
+	public Text contadorHearts;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	
+
+	}
+
+	public void clickEnElBoton(){
+		
+		this.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 7, ForceMode2D.Impulse);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// movimiento hacia la derecha
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.gameObject.transform.Translate (speed, 0f, 0f);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.gameObject.transform.Translate (-speed, 0f, 0f);
+		}
+		// salto
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 7, ForceMode2D.Impulse);
+
+		}
+
+
+
+		
+	}
+
+	// Collision 
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Coins"){
+
+			coins = coins + 1;
+
+			contadorCoins.text = coins.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+		}
+
+		if (coll.gameObject.tag == "Hearts"){
+
+			hearts = hearts + 1;
+
+			contadorHearts.text = hearts.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+		}
+	}
+
+}
+
+	
+
